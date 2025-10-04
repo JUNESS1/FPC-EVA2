974646c4929ab32edab45021acd816be4b45aa49
JUNESSY1 <junetogro1102@gmail.com>
2025-10-03 21:41:53 -0600

    Add files via upload

---
+++ ISC_FPC_EVA2_8_FOR_25550471/src/eva2_8_for/EVA2_8_FOR.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_8_for;
+
+/**
+ *
+ * @author equipo
+ */
+public class EVA2_8_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //3 parametros 
+        //variable y valor de inicio
+        //hasta donde llegas CONDICION ---> TIENE QUE SER VERDAD
+        //CUANDO ES FALSO EL FOR SE DETIENE
+        //incremento o decremento
+        //SUMAR O RESTAR 
+        //OPERADORES: AÑADIR 1 O RESTAR 1
+        // ++  --
+        // i++ SIGNIFICA SUMARLE 1 AL VALOR DE LA i
+        /*for(int i = 1; i <= 10; i ++){
+            System.out.println("HOLA MUNDO!!");
+            
+           
+        }*/ 
+         for(int i = 10; i >= 1; i --){
+            System.out.println("HOLA MUNDO!!");
+    }
+    // i-- SIGNIFICA RESTARLE 1 AL VALOR DE LA i
+}
+}
