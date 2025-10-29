239151d25f497fab7c1284a39ac9effaa9ed1abd
JUNESSY1 <junetogro1102@gmail.com>
2025-10-28 22:04:24 -0600

    Add files via upload

---
+++ ISC_FPC_EVA2_18_WHILE_25550471/src/eva2_18_while/EVA2_18_WHILE.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_18_while;
+
+/**
+ *
+ * @author equipo
+ */
+public class EVA2_18_WHILE {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=10){//se repite si la condicion es verdad
+            System.out.println("JUNESSY TORRES");
+            i++;
+        }
+        //SIMULAR UN FOR CON UN DO WHILE
+        System.out.println("CICLO DO WHILE ------------");
+        i = 1;
+        do{
+            System.out.println("JUNESSY TORRES");
+            i++;
+        }while(i<=10);
+        
+     
+    }
+    
+}
